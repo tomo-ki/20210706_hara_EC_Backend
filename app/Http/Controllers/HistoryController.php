66533a3ce20ca44185c60b3d2c0b87a460b3c601
tomo-ki <69376551+tomo-ki@users.noreply.github.com>
2021-07-30 20:39:05 +0900

    update

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -13,26 +13,13 @@ class HistoryController extends Controller
     {
         $histories = History::all();
         foreach ($histories as $history) {
-            $order =  Order::where('id', $history->order_id)->first();
-            $history->user_id = $order->user_id;
-        }
-        return response()->json([
-            'data' => $histories
-        ], 200);
-    }
-    public function show($userId)
-    {
-        $histories = History::all();
-        foreach ($histories as $history) {
             $order =  Order::where('uuid', $history->order_uuid)->first();
             $product = Product::where('id', $history->product_id)->first();
             $history->user_id = $order->user_id;
             $history->name = $product->name;
-            $history->created_at = $product->created_at;
             $history->image_path = $product->image_path;
             $history->price = $product->price;
         }
-        $histories = $histories->where('user_id', $userId);
         return response()->json([
             'data' => $histories
         ], 200);
